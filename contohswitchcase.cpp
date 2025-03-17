cc856d24c0b5e68eeb1ae799fe2a2d5bc7bfbaef
Haffi24 <haffisaefulloh@gmail.com>
2025-03-17 16:17:49 +0700

    merapikan switch case menggunakan format dokumen

--- contohswitchcase.cpp
+++ contohswitchcase.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 using namespace std;
 
-double hitungIPK(double total, int sks,)
+double hitungIPK(double total, int sks, )
 {
     return total / sks;
 }
 
-string predikat (double ipk ){
-    if (ipk < 10){
+string predikat(double ipk)
+{
+    if (ipk < 10)
+    {
         return "Memuaskan";
     }
-    else{
+    else
+    {
         return "cumlaude";
     }
 }
-
-
-
 
 int main()
 {
@@ -24,9 +24,9 @@ int main()
     int sks;
     double total;
 
-    do 
+    do
     {
-        cout << "Pilih Menu"<< endl;
+        cout << "Pilih Menu" << endl;
         cout << "1. Cek nilai IPK" << endl;
         cout << "2. cek Predikat Kelulusan" << endl;
         cout << "3. Keluar";
@@ -34,32 +34,26 @@ int main()
         cout << "Masukan Pilihan (1/2/3) : ";
         cin >> pilihan;
 
-    switch (pilihan)
-    {
-    case '1':
-        cout << "Masukan Total Nilai = ";
-        cin >> total;
-        cout << "Masukan Jumlah Sks = ";
-        cin >> sks;
+        switch (pilihan)
+        {
+        case '1':
+            cout << "Masukan Total Nilai = ";
+            cin >> total;
+            cout << "Masukan Jumlah Sks = ";
+            cin >> sks;
 
-        cout << "Nilai ipk =" << hitungIPK
-        (total, sks);
-        break;
-    case '2':
-        cout << "Predikat anda adalah" <<predikat(hitungIPK(total, sks))<< endl;
-        break;
+            cout << "Nilai ipk =" << hitungIPK(total, sks);
+            break;
+        case '2':
+            cout << "Predikat anda adalah" << predikat(hitungIPK(total, sks)) << endl;
+            break;
 
-    case '3':
-        break;
+        case '3':
+            break;
 
-    default:
-        cout <<"pilihan anda salah" << endl
-        break;
-    
-    default:
-        break;
-    }    
-    } while (pilihan !=3);   
-    
-
+        default:
+            cout << "pilihan anda salah" << endl break;
+            
+        }
+    } while (pilihan != 3);
 }
