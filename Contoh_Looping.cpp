4ebe4fed002f9ade3e448483c293d72180d3c0de
Haffi24 <haffisaefulloh@gmail.com>
2025-03-17 15:25:14 +0700

    Menambahkan Increament

--- Contoh_Looping.cpp
+++ Contoh_Looping.cpp
@@ -3,4 +3,23 @@
 #include <cstdlib>
 using namespace std;
 
+int main ()
+{
+    int n;
+    int perulanganWhile = 0;
+    int perulanganDo = 0;
 
+// Perulanagn Dengan for
+
+cout << "Perulangan for pencacah naik" << endl;
+for (n = 0; n < 5; n++)
+{
+    cout << "Nilai n = " << n << "Selamat Datang" << endl;
+}
+cout << "Nilai n terakhir = " << n << endl;
+cout << endl;
+
+
+
+
+}
